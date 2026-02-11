5299db38fbff39d823ff9d504b4acbaf5d817589
LauraLuzhen <ll.rodriguez@iesnervion.es>
2026-02-11 09:31:18 +0100

    Repositorios de Data terminados

--- ProyectoGrupoSinco/src/domain/interfaces/repositories/IUsuarioRepository.ts
+++ ProyectoGrupoSinco/src/domain/interfaces/repositories/IUsuarioRepository.ts
@@ -4,7 +4,4 @@ export interface IUsuarioRepository {
   getListaUsuarios(): Promise<clsUsuario[]>;
   getUsuarioPorId(idUsuario: number): Promise<clsUsuario>;
   getUsuarioPorNombre(nombre: string): Promise<clsUsuario>;
-  crearUsuario(usuarioNuevo: clsUsuario): Promise<number>;
-  actualizarUsuario(idUsuario: number, usuario: clsUsuario): Promise<number>;
-  eliminarUsuario(idUsuario: number): Promise<number>;
 }
