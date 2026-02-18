80378c4c4349e60bd95779f91d45a9a611199355
LauraLuzhen <ll.rodriguez@iesnervion.es>
2026-02-18 18:45:18 +0100

    cambios

--- Proyecto-Gruposinco/src/presentation/components/pedido-item-component/pedido-item-component.spec.ts
+++ Proyecto-Gruposinco/src/presentation/components/pedido-item-component/pedido-item-component.spec.ts
@@ -1,23 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { clsPedido } from '../../../domain/entities/clsPedido';
+import { CommonModule } from '@angular/common';
 
-import { PedidoItemComponent } from './pedido-item-component';
-
-describe('PedidoItemComponent', () => {
-  let component: PedidoItemComponent;
-  let fixture: ComponentFixture<PedidoItemComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PedidoItemComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(PedidoItemComponent);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+@Component({
+  selector: 'app-pedido-item',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './pedido-item-component.html',
+  styleUrls: ['./pedido-item-component.css'],
+})
+export class PedidoItemComponent {
+  @Input() pedido!: clsPedido;
+}
