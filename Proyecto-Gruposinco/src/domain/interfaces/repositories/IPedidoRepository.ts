52eaf684afc26147ef4161248e5404043186c4b2
manuelgarfia0 <manuel.garfia@iesnervion.es>
2026-02-18 12:36:22 +0100

    Nuevo dto creado y actualizado todo lo conveniente con crear pedido

--- Proyecto-Gruposinco/src/domain/interfaces/repositories/IPedidoRepository.ts
+++ Proyecto-Gruposinco/src/domain/interfaces/repositories/IPedidoRepository.ts
@@ -1,3 +1,4 @@
+import { CrearPedidoDto } from '../../dtos/CrearPedidoDto';
 import { clsPedido } from '../../entities/clsPedido';
 
 export interface IPedidoRepository {
@@ -5,7 +6,7 @@ export interface IPedidoRepository {
   getListaPedidosPorUsuario(idUsuario: number): Promise<clsPedido[]>;
   getListaPedidosPorProveedor(idProveedor: number): Promise<clsPedido[]>;
   getPedidoPorId(idPedido: number): Promise<clsPedido>;
-  crearPedido(pedidoNuevo: clsPedido): Promise<number>;
+  crearPedido(pedidoNuevo: CrearPedidoDto): Promise<number>;
   actualizarPedido(idPedido: number, pedido: clsPedido): Promise<number>;
   cambiarEstadoPedido(idPedido: number, nuevoEstado: string): Promise<number>;
   eliminarPedido(idPedido: number): Promise<number>;
