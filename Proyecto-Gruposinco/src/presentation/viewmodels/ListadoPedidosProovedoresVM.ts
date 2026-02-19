1e3ecad91ff3ddde6b9fa5852a38d7e0ed488c07
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2026-02-19 13:49:58 +0100

    Añadido el try-catch en listado pedidos, y listado productos

--- Proyecto-Gruposinco/src/presentation/viewmodels/ListadoPedidosProovedoresVM.ts
+++ Proyecto-Gruposinco/src/presentation/viewmodels/ListadoPedidosProovedoresVM.ts
@@ -6,14 +6,18 @@ import { clsPedido } from '../../domain/entities/clsPedido';
 export class ListadoPedidosProveedoresVM {
   pedidos = signal<clsPedido[]>([]);
   loading = signal<boolean>(false);
+  error = signal<string | null>(null); // 👈
 
   constructor(private getPedidosUC: GetPedidosUseCase) {}
 
   async cargarPedidos() {
     this.loading.set(true);
+    this.error.set(null);
     try {
       const lista = await this.getPedidosUC.getListaPedidos();
       this.pedidos.set(lista.filter(p => !p.Archivado));
+    } catch (e: any) {
+      this.error.set('No se pudieron cargar los pedidos. Inténtalo de nuevo.'); // 👈
     } finally {
       this.loading.set(false);
     }
